eda386557baac4a08d51d955fbe4cd75e0e419c1
thrx.de <coder@thrx.de>
2017-12-18 23:40:46 +0100

    initial

---
+++ src/ThrxDistanceSensorHC_SR04.cpp
@@ -0,0 +1,53 @@
+// Do not remove the include below
+#include "ThrxDistanceSensorHC_SR04.h"
+
+/*
+ HC-SR04 Ping distance sensor]
+ VCC to arduino 5v GND to arduino GND
+ Echo to Arduino pin 13 Trig to Arduino pin 12
+ Red POS to Arduino pin 11
+ Green POS to Arduino pin 10
+ 560 ohm resistor to both LED NEG and GRD power rail
+ More info at: http://goo.gl/kJ8Gl
+ Original code improvements to the Ping sketch sourced from Trollmaker.com
+ Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+ */
+
+#define trigPin 12
+#define echoPin 13
+
+//The setup function is called once at startup of the sketch
+void setup() {
+	Serial.begin(9600);
+	Serial.println("Distance Messprogramm");
+	Serial.println("thrx 0.3 vom 25.04.2016");
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+}
+
+// The loop function is called in an endless loop
+void loop() {
+	long duration, distance;
+	digitalWrite(trigPin, LOW);  // Added this line
+	delayMicroseconds(2); // Added this line
+	digitalWrite(trigPin, HIGH);
+	//  delayMicroseconds(1000); - Removed this line
+	delayMicroseconds(10); // Added this line
+	digitalWrite(trigPin, LOW);
+	duration = pulseIn(echoPin, HIGH);
+	distance = (duration / 2) / 29.1;
+	if (distance < 4) {  // This is where the LED On/Off happens
+//	    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+//	  digitalWrite(led2,LOW);
+	} else {
+//	    digitalWrite(led,LOW);
+//	    digitalWrite(led2,HIGH);
+	}
+	if (distance >= 200 || distance <= 0) {
+		Serial.println("Out of range: "+distance);
+	} else {
+		Serial.print(distance);
+		Serial.println(" cm");
+	}
+	delay(500);
+}
